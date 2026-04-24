40a6b399deb0f5a97aef1244e321d5a8e4145e36
Keenan Yuri Gottschalk Benetti <benettiescritorio@gmail.com>
2026-04-24 13:42:14 -0300

    Cria o projeto de batalha naval

---
+++ src/main/java/com/batalhaNaval/Main.java
@@ -0,0 +1,27 @@
+package com.batalhaNaval;
+
+public class Main {
+    static void main() {
+        //Batalha Naval:
+        //Objetivo Principal:Criar um programa de batalha naval
+        //Objetivos secundarios:
+            // conexão multiplayer na rede local via sockets
+            // Tela feita usando JavaFX
+            // Menu para escolha de multiplayer ou singleplayer
+            // tela de configurações do jogo
+            // Implementar drag e click para colocar os barcos no tabuleiro
+
+        int [][] tabuleiroPlayer1 = new int[10][10];
+        int [][] tabuleiroPlayer2 = new int[10][10];
+
+
+    }
+}
+
+public static void adicionarBarco(int L, int C, string Orientation){
+    //Permite adicionar um barco ao tabuleiro
+}
+
+public static void atirar(int L, int C){
+    //Permite verificar se um local do tabuleiro oponente tem um barco ou não
+}
